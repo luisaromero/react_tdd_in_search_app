1fc4f02eecd85135dc0c748fa52b080e6f8756d8
luisaromero <luisa.marcela.romero@gmail.com>
2023-02-15 22:10:16 -0300

    testing button searcch is or not disabled and configurate them

--- src/Components/github-search-page.js
+++ src/Components/github-search-page.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Typography, TextField, Button, Container, Grid, Box } from '@mui/material'; { }
 
 
-export const GithubSearchPage = () => (
-    <Container>
-        <Typography component="h1" variant="h3">github repositores list</Typography>
-        <Grid container spacing={2} justify="space-between">
-            <Grid item md={6} xs={12}>
-                <TextField fullWidth label="filter by" id="filterBy" />
+export const GithubSearchPage = () => {
+    const [isSearching, setIsSearching] = useState(false);
+
+    const hanldeClick = async () => {
+        setIsSearching(true)
+        await Promise.resolve()
+        setIsSearching(false)
+
+    }
+
+    return (
+        <Container>
+            <Typography component="h1" variant="h3">github repositores list</Typography>
+            <Grid container spacing={2} justify="space-between">
+                <Grid item md={6} xs={12}>
+                    <TextField fullWidth label="filter by" id="filterBy" />
+                </Grid>
+                <Grid item xs={12} md={3}>
+                    <Button fullWidth disabled={isSearching} onClick={hanldeClick} variant="contained" color="primary">Search</Button>
+                </Grid>
             </Grid>
-            <Grid item xs={12} md={3}>
-                <Button fullWidth variant="contained" color="primary">Search</Button>
-            </Grid>
-        </Grid>
-        <Box display="flex" alignItems={'center'} justifyContent="center" height={400}>
-            <Typography>Please provide a search option and click in the search button</Typography>
-        </Box>
-    </Container >
-)
+            <Box display="flex" alignItems={'center'} justifyContent="center" height={400}>
+                <Typography>Please provide a search option and click in the search button</Typography>
+            </Box>
+        </Container >
+    )
+}
 
 export default GithubSearchPage
--- src/Components/github-search-page.test.js
+++ src/Components/github-search-page.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { GithubSearchPage } from './github-search-page';
 
+beforeEach(() => render(<GithubSearchPage />))
+
+
 describe('when the GithubSearcis mounted', () => {
-    beforeEach(() => render(<GithubSearchPage />))
     it('must display the title', () => {
 
         expect(screen.getByRole('heading', { name: /github repositores list/i })).toBeInTheDocument()
@@ -22,5 +24,15 @@ describe('when the GithubSearcis mounted', () => {
     it('show the initial state message “Please provide a search option and click in the search button”', () => {
 
         expect(screen.getByText(/please provide a search option and click in the search button/i)).toBeInTheDocument()
+    })
+})
+
+describe('when the developer does a search', () => {
+    it('The search button should be disabled until the search is done', () => {
+        const btnSearch = screen.getByRole('button', { name: /search/i })
+        expect(btnSearch).not.toBeDisabled()
+        fireEvent.click(btnSearch)
+        expect(btnSearch).toBeDisabled()
+
     })
 })
