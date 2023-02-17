6c66c0de6ccc5a13e9790dc010e24840384ad9fa
luisaromero <luisa.marcela.romero@gmail.com>
2023-02-17 15:35:42 -0300

    test and render of table

--- src/Components/github-search-page.test.js
+++ src/Components/github-search-page.test.js
@@ -28,29 +28,39 @@ describe('when the GithubSearcis mounted', () => {
 })
 
 describe('when the developer does a search', () => {
+    const btnSearch = () => screen.getByRole('button', { name: /search/i })
+    const fireEventSearch = () => fireEvent.click(btnSearch())
     it('The search button should be disabled until the search is done', async () => {
-        const btnSearch = screen.getByRole('button', { name: /search/i })
-        expect(btnSearch).not.toBeDisabled()
-        fireEvent.click(btnSearch)
-        expect(btnSearch).toBeDisabled()
-        await waitFor(() => expect(btnSearch).not.toBeDisabled())
+        expect(btnSearch()).not.toBeDisabled()
+        fireEventSearch()
+        expect(btnSearch()).toBeDisabled()
+        await waitFor(() => expect(btnSearch()).not.toBeDisabled())
 
     })
 
     it('The The data should be displayed as a sticky table', async () => {
-        const btnSearch = screen.getByRole('button', { name: /search/i })
-        fireEvent.click(btnSearch)
+        fireEventSearch()
         await waitFor(() =>
             expect(screen.queryByText(/please provide a search option and click in the search button/i)).not.toBeInTheDocument())
         expect(screen.getByRole('table')).toBeInTheDocument()
     })
 
     it('The table headers must be contain Repository, stars, forks, open issues and updated at', async () => {
-        const btnSearch = screen.getByRole('button', { name: /search/i })
-        fireEvent.click(btnSearch)
+        fireEventSearch()
         const table = await screen.findByRole('table')
 
         const TableHeaders = within(table).getAllByRole('columnheader')
+
+
+        const [repository, stars, forks, openIssues, updatedAt] = TableHeaders
+
+        expect(TableHeaders).toHaveLength(5)
+        expect(repository).toHaveTextContent(/repository/i)
+        expect(stars).toHaveTextContent(/stars/i)
+        expect(forks).toHaveTextContent(/forks/i)
+        expect(openIssues).toHaveTextContent(/open issues/i)
+        expect(updatedAt).toHaveTextContent(/updated at/i)
+
 
     })
 })
