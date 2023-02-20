e8ae4b5f5d35b8829d25768746514bb06d8ce441
luisaromero <luisa.marcela.romero@gmail.com>
2023-02-20 16:44:46 -0300

    install msw

--- src/Components/github-search-page.test.js
+++ src/Components/github-search-page.test.js
@@ -1,6 +1,39 @@
 import React from 'react';
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
 import { fireEvent, render, screen, waitFor, within } from '@testing-library/react';
 import { GithubSearchPage } from './github-search-page';
+
+
+const server = setupServer(
+    // Describe network behavior with request handlers.
+    // Tip: move the handlers into their own module and
+    // import it across your browser and Node.js setups!
+    rest.get('/posts', (req, res, ctx) => {
+        return res(
+            ctx.json([
+                {
+                    id: 'f8dd058f-9006-4174-8d49-e3086bc39c21',
+                    title: `Avoid Nesting When You're Testing`,
+                },
+                {
+                    id: '8ac96078-6434-4959-80ed-cc834e7fef61',
+                    title: `How I Built A Modern Website In 2021`,
+                },
+            ]),
+        )
+    }),
+)
+
+// Enable request interception.
+beforeAll(() => server.listen())
+
+// Reset handlers so that each test could alter them
+// without affecting other, unrelated tests.
+afterEach(() => server.resetHandlers())
+
+// Don't forget to clean up afterwards.
+afterAll(() => server.close())
 
 
 beforeEach(() => render(<GithubSearchPage />))
@@ -142,6 +175,9 @@ describe('when the developer does a search', () => {
 
         expect(previousPage).toBeDisabled()
 
-
     })
+
+})
+
+describe('when the developer does a search without results', () => {
 })
